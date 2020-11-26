08fed43623bb048ae6d4ce510c7d4ca7d97068ea
Ilia Glotov <ilia.glotov@bk.ru>
2020-11-26 23:28:00 +0300

    First commit

---
+++ WebDriverLab/WebDriverLab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("WebDriverLab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDriverLab")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6454644b-e7f8-45b9-983b-5b02aeb05335")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebDriverLab/WebDriverLab/Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+
+namespace WebDriver
+{
+    public class Tests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://htc-online.ru");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            IWebElement accessoriesForSmartphonesButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//a[text() = 'Аксессуары для смартфонов']")));
+            accessoriesForSmartphonesButton.Click();
+
+            IWebElement audioButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//a[text() = 'аудио']")));
+            audioButton.Click();
+
+            IWebElement originalAdapterButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[text() = 'Оригинальный адаптер с Type C на 3,5 мм (DC M321)']")));
+            originalAdapterButton.Click();
+
+            IWebElement buyButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//a[text() = 'Купить']")));
+            buyButton.Click();
+
+            IWebElement goToBusketButton = wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[text() = 'Перейти в корзину']")));
+            goToBusketButton.Click();
+
+            IWebElement originalAdapterButtonInBusket = wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[text() = 'Оригинальный адаптер с Type C на 3,5 мм (DC M321)']")));
+
+            Assert.IsNotNull(originalAdapterButtonInBusket);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
